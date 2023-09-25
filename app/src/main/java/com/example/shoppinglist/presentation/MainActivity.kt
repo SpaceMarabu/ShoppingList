1313164cc5467437624de449296a549e82f86cd9
spacemarabu <qiberos@gmail.com>
2023-09-25 21:10:20 +0300

    step 4.3

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
 
@@ -10,13 +10,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        viewModel.shopList.observe(this){
+        viewModel.shopList.observe(this) {
             Log.d("MainAcivity", it.toString())
+            val item = it[0]
+            if (count++ == 0) {
+                viewModel.changeEnableState(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
